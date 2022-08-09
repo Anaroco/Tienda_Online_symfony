e1867f6daa8b6e0de18afa832cfb98154380802b
Anaroco <roblescortes@msn.com>
2022-08-09 19:12:37 +0200

    add products

--- src/Controller/ProductosCRUD.php
+++ src/Controller/ProductosCRUD.php
@@ -2,15 +2,44 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Producto;
+
  /**
  * @Route("/productos", name="database")
  */
 
 class ProductosCRUD
 {
-    private $productoService;
+    private $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
+/**
+ * @Route("/insertar", name="insertar")
+ */
+public function insertar()
+{
+   $producto = new Producto();
+   $producto->setTitulo("Camiseta Elvis");
+   $producto->setDescripcion("Camiseta todas las tallas chica/chico");
+   $producto->setPrecio(10.0);
+   $producto->setIva(21);
+   $producto->setFotos("elvis.png");
+   $producto->setFotoportada("rock.png");
+   $producto->setFichatecnica("ficha.pdf");
+
+   $this->entityManager->persist($producto);
+   $this->entityManager->flush();
+
+   return new Response('<html><h1>Soy los productos</h1></html>');
+}
    
 
 }
--- src/Services/ProductService.php
+++ src/Services/ProductService.php
@@ -1,29 +1,52 @@
 <?php
+    // namespace App\Services;
+    // use App\Entity\ProductoDTO;
+
+    // class ProductService
+    // {
+    //     public $productos=[];
+
+    //     public function __construct()
+    //     {
+    //         $producto = new ProductoDTO("Titulo", "pikachu.png","pikachu.png","ficha.pdf");
+    //         $this->productos[0]=$producto;
+    //         $producto = new ProductoDTO("Titulo 2", "pikachu.png","pikachu.png","ficha.pdf");
+    //         $this->productos[1]=$producto;
+    //         $producto = new ProductoDTO("Titulo 3", "pikachu.png","pikachu.png","ficha.pdf");
+    //         $this->productos[2]=$producto;
+    //         $producto = new ProductoDTO("Titulo 4", "pikachu.png","pikachu.png","ficha.pdf");
+    //         $this->productos[3]=$producto;
+    //     }
+    //     public function todosProductos()
+    //     {
+    //         return $this->productos;
+    //     }
+    //     public function unProducto($titulo)
+    //     {
+    //         return $this->productos[0];
+    //     }
+    // }
+
     namespace App\Services;
     use App\Entity\ProductoDTO;
+    use App\Repository\ProductoRepository;
 
     class ProductService
     {
-        public $productos=[];
+        public $productRepository;
 
-        public function __construct()
+        public function __construct(ProductoRepository $productRepository)
         {
-            $producto = new ProductoDTO("Titulo", "pikachu.png","pikachu.png","ficha.pdf");
-            $this->productos[0]=$producto;
-            $producto = new ProductoDTO("Titulo 2", "pikachu.png","pikachu.png","ficha.pdf");
-            $this->productos[1]=$producto;
-            $producto = new ProductoDTO("Titulo 3", "pikachu.png","pikachu.png","ficha.pdf");
-            $this->productos[2]=$producto;
-            $producto = new ProductoDTO("Titulo 4", "pikachu.png","pikachu.png","ficha.pdf");
-            $this->productos[3]=$producto;
+          $this->productRepository = $productRepository;
         }
         public function todosProductos()
         {
-            return $this->productos;
+            return $this->productRepository->findAll();
         }
-        public function unProducto($titulo)
+        public function unProducto($id)
         {
-            return $this->productos[0];
+            return $this->productRepository->find($id);
         }
     }
+
 ?>
